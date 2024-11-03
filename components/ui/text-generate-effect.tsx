9d73167724e85c5d8520428cfb765dea48fc454b
Sabiq Hashil <99120128+SabiqHashil@users.noreply.github.com>
2024-11-04 01:00:27 +0530

    "Update TextGenerateEffect component: changed font from bold to normal and adjusted text size for different screen sizes"

--- components/ui/text-generate-effect.tsx
+++ components/ui/text-generate-effect.tsx
@@ -51,9 +51,9 @@ export const TextGenerateEffect = ({
   };
 
   return (
-    <div className={cn("font-bold", className)}>
+    <div className={cn("font-normal", className)}>
       <div className="mt-4">
-        <div className=" dark:text-white text-black text-2xl leading-snug tracking-wide">
+        <div className=" dark:text-white text-black xl:text-2xl lg:text-2xl md:text-xl  text-sm leading-snug tracking-wide">
           {renderWords()}
         </div>
       </div>
